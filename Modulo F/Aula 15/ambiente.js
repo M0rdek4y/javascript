c492c4203573c0334769eec7970663195b30b5bf
M0rdek4y <morde1127@gmail.com>
2022-10-27 19:15:15 -0300

    Update
    Added - Modulo F/complete

---
+++ Modulo F/Aula 15/ambiente.js
@@ -0,0 +1,8 @@
+let num = [5, 8, 2, 9, 3]
+num[5] = 7
+num.push(17)
+var tam = num.length
+console.log(`Nosso vetor é ${num} e ele possui ${tam} caracteres.`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+console.log(`O ultimo valor do vetor é ${num.sort()}`)
+console.log(`O valor escolhido foi ${num.sort()}.`)
---
+++ Modulo F/Aula 16/funcao01.js
@@ -0,0 +1,10 @@
+function parimp(n) {
+    if (n%2==0) {
+        return 'Par!'
+    } else {
+        return 'Impar!'
+    }
+}
+
+let num = parimp(8)
+console.log(num)
